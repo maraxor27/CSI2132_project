f6f1e436c0da2f32caea07bdba83bdb81d70b08a
Michael Malek <malek.mjm2@gmail.com>
2022-04-04 18:25:08 -0400

    completed branch api endpoint

--- src/router/__init__.py
+++ src/router/__init__.py
@@ -2,10 +2,12 @@ from flask import Blueprint
 from flask_restx import Api
 
 from .login import loginBlueprint, User
-from .patient import patientNameSpace
+#from .patient import patientNameSpace
+from .branch import branchNameSpace
 
 
 apiv2Blueprint = Blueprint("apiv2", __name__, url_prefix="/api/v2")
 api = Api(apiv2Blueprint, version='2.0', doc='/doc/')
-api.add_namespace(patientNameSpace)
+#api.add_namespace(patientNameSpace)
+api.add_namespace(branchNameSpace)
 #api.add_namespace(decompilerNamespace)
